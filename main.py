dcdc96a8ecf0461181d3c2b38c8daf9cad93f2b1
LieneN <>
2021-06-07 07:33:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print('Uzrakstīt programmu, kur izvadīti uz ekrāna skaitļi 1 līdz 20, to kubu un kvadrātu starpība. ')
+for i in range(1,20+1):
+  print(i, end=' ')
+  a = i**2
+  b = i**3
+print('\n',b-a)
+  
